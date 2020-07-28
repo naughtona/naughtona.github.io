43edd9c266f38418ed5862fc2c1ba25aeb5a7773
Andrew Naughton <naughtona111@gmail.com>
2020-07-29 01:07:29 +1000

    add background colours, resize connorsrun, dims

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let mybutton = document.getElementById("topButton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
